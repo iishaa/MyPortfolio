ffcff8590995e204153c0ae9c7e187115476bf59
isha <isharaj948@gmail.com>
2023-08-20 13:51:41 +0530

    commit portfolio

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -98,7 +98,7 @@ const ComputersCanvas = () => {
         enableZoom={false}
         maxPolarAngle={Math.PI/2}
         minPolarAngle={Math.PI/2} />
-        <Computers isMobile={isMobile}/>
+        <Computers/>
       </Suspense>
       <Preload all/>
     </Canvas> 
